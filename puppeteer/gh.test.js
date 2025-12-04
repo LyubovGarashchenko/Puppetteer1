e43a493dd17652350dc6d8333e08d436305e33fe
Lyubov Garashchenko <lyubov-balabina@yandex.ru>
2025-12-04 22:50:07 +0300

    Tasts fall. Why?

--- puppeteer/gh.test.js
+++ puppeteer/gh.test.js
@@ -1,8 +1,8 @@
+const puppeteer = require("puppeteer");
 let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://github.com/team");
 });
 
 afterEach(() => {
@@ -10,6 +10,12 @@ afterEach(() => {
 });
 
 describe("Github page tests", () => {
+
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto("https://github.com/team");
+  }, 5000);
+  
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
@@ -31,4 +37,30 @@ describe("Github page tests", () => {
     const actual = await page.$eval(btnSelector, link => link.textContent);
     expect(actual).toContain("Sign up for free")
   });
+});
+
+describe("Second task three tests", () => {
+  afterEach(() => {
+    page.close();
+  });
+
+  test("Check title Healthcare", async () => {
+    await page.goto("https://github.com/solutions/industry/healthcare");
+    const actual = await page.$eval("main a", (link) =>
+      link.getAttribute("href")
+    );
+    expect(actual).toEqual("#hero-section-brand-heading");
+  }, 4000);
+
+  test("Check title Trending", async () => {
+    await page.goto("https://github.com/trending");
+    const title = await page.title();
+    expect(title).toContain("Trending · GitHub");
+  }, 2000);
+
+  test("Check title AI", async () => {
+    await page.goto("https://github.com/resources/articles?topic=ai");
+    const title = await page.title();
+    expect(title).toContain("GitHub Articles · GitHub");
+  }, 4000);
 });
